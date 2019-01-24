d83768dc47192328120ad0e26761e5c954cd4b56
Ittipon Teerapruettikulchai <ittipon.bay@gmail.com>
2019-01-25 02:55:44 +0700

    Add test content

---
+++ TestMissingPrefabs/Assets/Scripts/TestDatabase.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TestDatabase", menuName = "Create/TestDatabase")]
+public class TestDatabase : ScriptableObject
+{
+    public TestEntity[] testEntities;
+}
---
+++ TestMissingPrefabs/Assets/Scripts/TestEntity.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class TestEntity : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnValidate()
+    {
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(gameObject);
+#endif
+    }
+}
